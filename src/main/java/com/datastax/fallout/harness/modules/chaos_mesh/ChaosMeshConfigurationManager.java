f74aed5e0927ceacb9ad6cb60e5a470a24b4601f
Guy Bolton King <guy.boltonking@datastax.com>
2020-05-26 23:12:48 +0100

    Extract chaos-mesh resources from JAR

--- src/main/java/com/datastax/fallout/harness/modules/chaos_mesh/ChaosMeshConfigurationManager.java
+++ src/main/java/com/datastax/fallout/harness/modules/chaos_mesh/ChaosMeshConfigurationManager.java
@@ -15,12 +15,18 @@
  */
 package com.datastax.fallout.harness.modules.chaos_mesh;
 
+import java.io.File;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.jar.JarFile;
 
 import com.google.auto.service.AutoService;
 import com.google.common.collect.ImmutableList;
@@ -40,16 +46,15 @@ import com.datastax.fallout.ops.providers.FileProvider;
 import com.datastax.fallout.ops.providers.KubeControlProvider;
 import com.datastax.fallout.ops.provisioner.kubernetes.KindProvisioner;
 import com.datastax.fallout.util.Duration;
+import com.datastax.fallout.util.Exceptions;
 
 @AutoService(ConfigurationManager.class)
 public class ChaosMeshConfigurationManager extends ConfigurationManager
 {
     private static final String prefix = "fallout.configuration.management.chaos_mesh.";
 
-    private static final Path CHAOS_MESH_CRD = Paths.get(
-        Resources.getResource(ChaosMeshConfigurationManager.class, "crd.yaml").getPath());
-    private static final Path CHAOS_MESH_HELM_CHART = Paths.get(
-        Resources.getResource(ChaosMeshConfigurationManager.class, "helm-chart").getPath());
+    private static final String CHAOS_MESH_CRD = "crd.yaml";
+    private static final String CHAOS_MESH_HELM_CHART = "helm-chart";
 
     private static final Optional<String> NAMESPACE = Optional.of("chaos-mesh");
 
@@ -103,9 +108,25 @@ public class ChaosMeshConfigurationManager extends ConfigurationManager
         return Set.of(ChaosMeshProvider.class);
     }
 
+    private Path configFilePath(String path)
+    {
+        return getNodeGroup().getLocalArtifactPath().resolve("chaos-mesh").resolve(path);
+    }
+
+    private void writeConfigFiles(String rootPath)
+    {
+        walkResourceTree(this.getClass(), rootPath, (path, content) -> Exceptions.runUncheckedIO(() -> {
+            Files.createDirectories(configFilePath(path).getParent());
+            Files.write(configFilePath(path), content);
+        }));
+    }
+
     @Override
     public boolean configureImpl(NodeGroup nodeGroup)
     {
+        writeConfigFiles(CHAOS_MESH_CRD);
+        writeConfigFiles(CHAOS_MESH_HELM_CHART);
+
         if (!deployChaosMeshCRD())
         {
             logger.error("Failed to deploy chaos mesh crd");
@@ -122,13 +143,15 @@ public class ChaosMeshConfigurationManager extends ConfigurationManager
             marker -> nodeGroup.findFirstRequiredProvider(FileProvider.LocalFileProvider.class).getFullPath(marker));
 
         return nodeGroup.findFirstRequiredProvider(KubeControlProvider.class).inNamespace(NAMESPACE,
-            namespacedKubeCtl -> namespacedKubeCtl.installHelmChart(CHAOS_MESH_HELM_CHART, flags, optionsFile));
+            namespacedKubeCtl -> namespacedKubeCtl.installHelmChart(configFilePath(CHAOS_MESH_HELM_CHART), flags,
+                optionsFile));
     }
 
     private boolean deployChaosMeshCRD()
     {
-        return KubernetesManifestConfigurationManager.applyAndWaitForManifest(getNodeGroup(), NAMESPACE, CHAOS_MESH_CRD,
-            Utils.readStringFromFile(CHAOS_MESH_CRD.toFile()),
+        return KubernetesManifestConfigurationManager.applyAndWaitForManifest(getNodeGroup(), NAMESPACE,
+            configFilePath(CHAOS_MESH_CRD),
+            Utils.readStringFromFile(configFilePath(CHAOS_MESH_CRD).toFile()),
             KubernetesManifestSpec.ManifestWaitOptions.fixedDuration(Duration.seconds(5)));
     }
 
@@ -156,7 +179,70 @@ public class ChaosMeshConfigurationManager extends ConfigurationManager
     public boolean unconfigureImpl(NodeGroup nodeGroup)
     {
         return nodeGroup.findFirstRequiredProvider(KubeControlProvider.class)
-            .inNamespace(NAMESPACE, namespacedKubeCtl -> namespacedKubeCtl.uninstallHelmChart(CHAOS_MESH_HELM_CHART) &&
-                namespacedKubeCtl.deleteResource(CHAOS_MESH_CRD).waitForSuccess());
+            .inNamespace(NAMESPACE, namespacedKubeCtl -> namespacedKubeCtl.uninstallHelmChart(
+                configFilePath(CHAOS_MESH_HELM_CHART)) &&
+                namespacedKubeCtl.deleteResource(configFilePath(CHAOS_MESH_CRD)).waitForSuccess());
+    }
+
+    private static void walkJarResourceTree(String path, URL resourceUrl, Consumer<String> pathConsumer)
+    {
+        final var jarAndResourcePath = resourceUrl.getPath().split("!", 2);
+        final var jarPath = jarAndResourcePath[0].substring(5);
+        final var resourcePath = jarAndResourcePath[1].substring(1);
+
+        final var jarFile = Exceptions.getUncheckedIO(() -> new JarFile(new File(jarPath)));
+        final var entries = jarFile.entries();
+
+        while (entries.hasMoreElements())
+        {
+            final var entry = entries.nextElement();
+            final var entryName = entry.getName();
+            if (!entry.isDirectory() && entryName.startsWith(resourcePath))
+            {
+                final var entryPath = path + entryName.substring(resourcePath.length());
+                pathConsumer.accept(entryPath);
+            }
+        }
+
+    }
+
+    private static void walkFileResourceTree(String path, File resourceFile, Consumer<String> pathConsumer)
+    {
+        if (resourceFile.isDirectory())
+        {
+            for (var file : resourceFile.listFiles())
+            {
+                walkFileResourceTree(path + "/" + file.getName(), file, pathConsumer);
+            }
+        }
+        else
+        {
+            pathConsumer.accept(path);
+        }
+    }
+
+    private static void walkResourceTree(Class<?> clazz, String path, BiConsumer<String, byte[]> pathAndContentConsumer)
+    {
+        final Consumer<String> pathConsumer = path_ -> pathAndContentConsumer.accept(path_,
+            Exceptions.getUncheckedIO(() -> Resources.toByteArray(clazz.getResource(path_))));
+
+        final var resourceUrl = clazz.getResource(path);
+
+        if (resourceUrl.getProtocol().equals("jar"))
+        {
+            walkJarResourceTree(path, resourceUrl, pathConsumer);
+        }
+        else
+        {
+            walkFileResourceTree(path, new File(Exceptions.getUnchecked(() -> resourceUrl.toURI())), pathConsumer);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        walkResourceTree(ChaosMeshConfigurationManager.class, "crd.yaml",
+            (path, content) -> System.out.println(">>> " + path + "\n" + new String(content, StandardCharsets.UTF_8)));
+        walkResourceTree(ChaosMeshConfigurationManager.class, "helm-chart",
+            (path, content) -> System.out.println(">>> " + path + "\n" + new String(content, StandardCharsets.UTF_8)));
     }
 }
