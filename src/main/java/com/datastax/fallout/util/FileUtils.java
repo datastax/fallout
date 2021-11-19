0d8f5e35167c09baebf79af264244cfb66d851b1
Guy Bolton King <guy@waftex.com>
2021-11-19 19:00:43 +0000

    Add new FileUtils.with*FileContent* API
    This allows us to handle compressed files without unzipping them

--- src/main/java/com/datastax/fallout/util/FileUtils.java
+++ src/main/java/com/datastax/fallout/util/FileUtils.java
@@ -28,6 +28,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.zip.GZIPInputStream;
@@ -39,6 +42,9 @@ import java.util.zip.ZipInputStream;
 import com.google.common.base.Verify;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
+
+import static com.google.common.io.Files.getFileExtension;
+import static com.google.common.io.Files.getNameWithoutExtension;
 
 public class FileUtils
 {
@@ -248,5 +254,100 @@ public class FileUtils
     public static String readString(InputStream inputStream)
     {
         return Exceptions.getUncheckedIO(() -> new String(inputStream.readAllBytes(), StandardCharsets.UTF_8));
+    }
+
+    public interface FileContentsConsumer<R>
+    {
+        /** Process the contents of a file at filePath, optionally with an archivePath if the filePath
+         *  is an archive */
+        R apply(Path filePath, Optional<String> archivePath, InputStream contents);
+
+        default R apply(Path filePath, InputStream contents)
+        {
+            return apply(filePath, Optional.empty(), contents);
+        }
+    }
+
+    public static <R> List<R> withZippedFileContents(Path zipFilePath, FileContentsConsumer<R> consumer)
+    {
+        return Exceptions.getUncheckedIO(() -> {
+            try (var zipFile = new ZipFile(zipFilePath.toFile(), StandardCharsets.UTF_8))
+            {
+                return zipFile.stream()
+                    .filter(entry -> !entry.isDirectory())
+                    .map(entry -> Exceptions.getUncheckedIO(() -> {
+                        try (var inputStream = zipFile.getInputStream(entry))
+                        {
+                            return consumer.apply(zipFilePath, Optional.of(entry.getName()), inputStream);
+                        }
+                    }))
+                    .collect(Collectors.toList());
+            }
+        });
+    }
+
+    public static <R> R withFileContent(Path filePath, FileContentsConsumer<R> consumer)
+    {
+        return Exceptions.getUncheckedIO(() -> {
+            try (var inputStream = Files.newInputStream(filePath))
+            {
+                return consumer.apply(filePath, inputStream);
+            }
+        });
+    }
+
+    public static <R> R withGzippedFileContent(Path gzipFilePath, FileContentsConsumer<R> consumer)
+    {
+        return withFileContent(gzipFilePath, (fileName, archivePath, inputStream) -> {
+            return Exceptions.getUncheckedIO(() -> {
+                try (var gzipInputStream = new GZIPInputStream(inputStream))
+                {
+                    return consumer.apply(gzipFilePath, gzipInputStream);
+                }
+            });
+        });
+    }
+
+    /** Return whether path has the extension _and_ if the path without that extension is accepted
+     *  by matches */
+    private static boolean matchesPathWithoutExtension(Predicate<Path> matches, Path path, String extension)
+    {
+        final var pathExtension = getFileExtension(path.toString()).toLowerCase(Locale.ROOT);
+
+        return pathExtension.equals(extension) &&
+            matches.test(
+                path.getParent().resolve(getNameWithoutExtension(path.getFileName().toString())));
+    }
+
+    /** Recurse rootPath.  If a path (with zip and gz extensions removed) satisfies consume, then the path
+     *  (with no extensions removed) is passed to consumer with a stream of its uncompressed contents.   Returns
+     *  whether any files were found. */
+    public static <R> List<R> withMaybeCompressedFileContentsInPath(Path rootPath,
+        Predicate<Path> consume,
+        FileContentsConsumer<R> consumer)
+    {
+        return Exceptions.getUncheckedIO(() -> {
+            try (Stream<Path> pathStream = Files.walk(rootPath))
+            {
+                return pathStream
+                    .filter(path -> !Files.isDirectory(path))
+                    .flatMap(path -> {
+                        if (matchesPathWithoutExtension(consume, path, "zip"))
+                        {
+                            return withZippedFileContents(path, consumer).stream();
+                        }
+                        else if (matchesPathWithoutExtension(consume, path, "gz"))
+                        {
+                            return Stream.of(withGzippedFileContent(path, consumer));
+                        }
+                        else if (consume.test(path))
+                        {
+                            return Stream.of(withFileContent(path, consumer));
+                        }
+                        return Stream.empty();
+                    })
+                    .collect(Collectors.toList());
+            }
+        });
     }
 }
