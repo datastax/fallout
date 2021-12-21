100dde824d809da42ee94dd760b16b11fa8e52ea
Guy Bolton King <guy@waftex.com>
2021-12-21 15:39:29 +0000

    Use record for Aggregate

--- src/main/java/com/datastax/fallout/util/cockpit/Aggregate.java
+++ src/main/java/com/datastax/fallout/util/cockpit/Aggregate.java
@@ -17,33 +17,17 @@ package com.datastax.fallout.util.cockpit;
 
 import javax.annotation.Nullable;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.google.auto.value.AutoValue;
 
-@AutoValue
-@AutoValue.CopyAnnotations
-@JsonSerialize(as = Aggregate.class)
 @JsonPropertyOrder({"component", "category", "name", "value", "node", "phase"})
-public abstract class Aggregate<T>
-{
-    public abstract String getComponent();
-
-    public abstract String getCategory();
-
-    public abstract String getName();
-
-    public abstract T getValue();
-
-    @Nullable
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    public abstract String getNode();
-
-    @Nullable
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    public abstract String getPhase();
+public record Aggregate<T> (
+    String component,
+    String category,
+    String name,
+    T value,
+    @Nullable @JsonInclude(JsonInclude.Include.NON_NULL) String node,
+    @Nullable @JsonInclude(JsonInclude.Include.NON_NULL) String phase) {
 
     public static <T> Aggregate<T> of(String component, String category, String name, T value)
     {
@@ -55,15 +39,14 @@ public abstract class Aggregate<T>
         return Aggregate.of(component, category, name, value, node, null);
     }
 
-    @JsonCreator
     public static <T> Aggregate<T> of(String component, String category, String name, T value, String node,
         String phase)
     {
-        return new AutoValue_Aggregate<T>(component, category, name, value, node, phase);
+        return new Aggregate<T>(component, category, name, value, node, phase);
     }
 
     public Aggregate<T> withPhase(String phase)
     {
-        return Aggregate.of(getComponent(), getCategory(), getName(), getValue(), getNode(), phase);
+        return Aggregate.of(component(), category(), name(), value(), node(), phase);
     }
 }
