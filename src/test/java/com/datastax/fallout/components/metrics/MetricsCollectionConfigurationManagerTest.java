12c7fb88cb3c3c3015e4aa2e72f3227003a005cf
himanshudatastax <himanshu.chaudhary4@ibm.com>
2026-06-10 14:27:41 +0530

    Successfully executed ./gradlew :spotlessApply again

--- src/test/java/com/datastax/fallout/components/metrics/MetricsCollectionConfigurationManagerTest.java
+++ src/test/java/com/datastax/fallout/components/metrics/MetricsCollectionConfigurationManagerTest.java
@@ -28,7 +28,6 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import com.datastax.fallout.ops.NodeGroup;
 import com.datastax.fallout.util.ResourceUtils;
 
 import static com.datastax.fallout.assertj.Assertions.assertThat;
@@ -38,7 +37,6 @@ import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
-import static org.mockito.Mockito.mock;
 
 class MetricsCollectionConfigurationManagerTest
 {
@@ -92,7 +90,7 @@ class MetricsCollectionConfigurationManagerTest
     {
         MetricsCollectionConfigurationManager metricsCollectionConfigurationManager =
             new MetricsCollectionConfigurationManager(instantSupplier);
-        
+
         String jsonResponse =
             metricsCollectionConfigurationManager.executeGetRequestForJsonContent(metricName, prometheusUrl());
 
@@ -125,7 +123,7 @@ class MetricsCollectionConfigurationManagerTest
         Supplier<Instant> instantSupplier = nowReference::get;
         MetricsCollectionConfigurationManager metricsCollectionConfigurationManager =
             new MetricsCollectionConfigurationManager(instantSupplier);
-        
+
         String metricName = "metric_a";
         Instant end = start.plus(Duration.ofSeconds(100));
         nowReference.set(end);
