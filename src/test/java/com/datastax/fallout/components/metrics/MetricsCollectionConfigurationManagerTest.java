17c601f9e070bda8565f677ac11b885f12ee87ad
himanshudatastax <himanshu.chaudhary4@ibm.com>
2026-06-10 14:11:41 +0530

    removing un-necessary
    Removed commented-out configuration lines from tests.

--- src/test/java/com/datastax/fallout/components/metrics/MetricsCollectionConfigurationManagerTest.java
+++ src/test/java/com/datastax/fallout/components/metrics/MetricsCollectionConfigurationManagerTest.java
@@ -92,8 +92,7 @@ class MetricsCollectionConfigurationManagerTest
     {
         MetricsCollectionConfigurationManager metricsCollectionConfigurationManager =
             new MetricsCollectionConfigurationManager(instantSupplier);
-        // metricsCollectionConfigurationManager.configureImpl(mock(NodeGroup.class));
-
+        
         String jsonResponse =
             metricsCollectionConfigurationManager.executeGetRequestForJsonContent(metricName, prometheusUrl());
 
@@ -110,7 +109,6 @@ class MetricsCollectionConfigurationManagerTest
     {
         MetricsCollectionConfigurationManager metricsCollectionConfigurationManager =
             new MetricsCollectionConfigurationManager(instantSupplier);
-        // metricsCollectionConfigurationManager.configureImpl(mock(NodeGroup.class));
 
         assertThatThrownBy(
             () -> metricsCollectionConfigurationManager.executeGetRequestForJsonContent(nonExistingMetricName,
@@ -127,8 +125,7 @@ class MetricsCollectionConfigurationManagerTest
         Supplier<Instant> instantSupplier = nowReference::get;
         MetricsCollectionConfigurationManager metricsCollectionConfigurationManager =
             new MetricsCollectionConfigurationManager(instantSupplier);
-        // metricsCollectionConfigurationManager.configureImpl(mock(NodeGroup.class));
-
+        
         String metricName = "metric_a";
         Instant end = start.plus(Duration.ofSeconds(100));
         nowReference.set(end);
