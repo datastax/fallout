022df7239b27b56db26af2477bd7e4be3b739986
himanshudatastax <himanshu.chaudhary4@ibm.com>
2026-06-12 18:00:32 +0530

    Adding NumPy installation in workflow

--- src/main/java/com/datastax/fallout/service/FalloutConfiguration.java
+++ src/main/java/com/datastax/fallout/service/FalloutConfiguration.java
@@ -705,12 +705,18 @@ public class FalloutConfiguration extends Configuration
             updateLogFilename(appender::getArchivedLogFilenamePattern, appender::setArchivedLogFilenamePattern);
         });
 
-        final var serverFactory = (DefaultServerFactory) getServerFactory();
-        final var requestLogFactory = (LogbackAccessRequestLogFactory) serverFactory.getRequestLogFactory();
-        modifyAppenders(FileAppenderFactory.class, requestLogFactory.getAppenders(), appender -> {
-            updateLogFilename(appender::getCurrentLogFilename, appender::setCurrentLogFilename);
-            updateLogFilename(appender::getArchivedLogFilenamePattern, appender::setArchivedLogFilenamePattern);
-        });
+        if (getServerFactory() instanceof DefaultServerFactory)
+        {
+            final var serverFactory = (DefaultServerFactory) getServerFactory();
+            if (serverFactory.getRequestLogFactory() instanceof LogbackAccessRequestLogFactory)
+            {
+                final var requestLogFactory = (LogbackAccessRequestLogFactory) serverFactory.getRequestLogFactory();
+                modifyAppenders(FileAppenderFactory.class, requestLogFactory.getAppenders(), appender -> {
+                    updateLogFilename(appender::getCurrentLogFilename, appender::setCurrentLogFilename);
+                    updateLogFilename(appender::getArchivedLogFilenamePattern, appender::setArchivedLogFilenamePattern);
+                });
+            }
+        }
     }
 
     /** Disables all configured loggers and sets logging to console */
